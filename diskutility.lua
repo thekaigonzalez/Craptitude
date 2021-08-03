4a33869530a823c71603e8f869c620c5e5ac59ea
kaigonzalez <gkai70264@gmail.com>
2021-08-02 20:55:20 -0400

    Add Documentations and Diskutility

--- diskutility.lua
+++ diskutility.lua
@@ -6,7 +6,7 @@ pser:argument("DISK", "the disk to run. This is REQUIRED!")
 
 pser:flag("-v --verbose")
 pser:option("-e --entry", "The Entry to run the file at. Either [main|any]", "any")
-pser:option("-i --iargs", "Adds these arguments to the entry as an array."):count("*")
+pser:option("-A --Args", "Adds these arguments to the entry as an array."):count("*")
 local args = pser:parse()
 
 if args["verbose"] then
